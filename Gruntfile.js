06040ba02aa7ea8a61b3941715af30b45fccdcfd
Richard Chen <masterking828@gmail.com>
2014-04-13 22:24:06 -0400

    debug option

--- Gruntfile.js
+++ Gruntfile.js
@@ -423,7 +423,7 @@ module.exports = function (grunt) {
         configFile: "node_modules/protractor/referenceConf.js", // Default config file
         keepAlive: true, // If false, the grunt process stops when the test fails.
         noColor: false, // If true, protractor will not use colors in its output.
-        //debug: true,
+        debug: true,
         args: {
           // Arguments passed to the command
         }
